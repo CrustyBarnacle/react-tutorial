e3854895244552f8f76c7e7917270bbb6cf2f387
Mario Burgos <CrustyBarnacle@users.noreply.github.com>
2018-11-21 14:58:28 -0800

    Update Api.js
    Adds componentDidMount() to call wikipedia API.

--- src/Api.js
+++ src/Api.js
@@ -5,7 +5,28 @@ class App extends Component {
         data: []
     };
     
+    // Code runs after the component is added to the DOM
+    componentDidMount() {
+        const url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*";
+        
+        fetch(url)
+            .then(result => result.json())
+            .then(result => {
+                this.setState({
+                    data: result
+                })
+            });
+    }
     
+    render() {
+        const { data } = this.state;
+        
+        const result = data.map((entry, index) => {
+            return <li key={entry}>{entry}</li>;
+        });
+        
+        return <ul>{result}</ul>
+    }
 }
 
 export default App;
